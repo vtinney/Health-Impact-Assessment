b7b83f8f815203028672b68a8f2a71cb56f736bd
Veronica Southerland <vtinney@gwu.edu>
2019-03-05 10:11:25 -0500

    Add files via upload

---
+++ interactive maps.R
@@ -0,0 +1,63 @@
+# packages ----------------------------------------------------------------
+library(dplyr)
+library(rmapshaper)
+library(maptools)
+library(highcharter)
+library(geojsonio)
+library(readr)
+library(viridis)
+library(purrr)
+library(rgdal)
+library(raster)
+library(highcharter)
+library(geojson)
+
+# data --------------------------------------------------------------------
+
+#import csv data file
+setwd("C:/Users/vtinney/GoogleDrive/EDF_shared/Rates/Excel/CHIS/")
+data <- read.csv('asthmac.csv')
+data <- rename(data, value = Comb_Est)
+data <- rename(data, zip = ZCTA5CE10)
+
+
+#import shape file
+setwd("C:/Users/vtinney/GoogleDrive/EDF_shared/Population/BA_Shape/bay/")
+zip <- readOGR(dsn=getwd(), layer = "BA_zip")
+map <- geojson_list(zip)
+
+# colors
+no_classes <- 6
+
+colors <- viridis(no_classes + 2) %>% 
+  rev() %>% 
+  head(-1) %>% 
+  tail(-1) %>% 
+  gsub("FF$", "", .)
+
+# brks <- quantile(data$value, probs = seq(0, 1, length.out = no_classes + 1))
+brks <- c(min(data$value), c(0.114,0.135,0.154,0.164,0.183), max(data$value))
+
+highchart(type = "map") %>% 
+  # data part
+  hc_add_series(mapData = map, data = data, type = "map",
+                joinBy = c("ZCTA5CE10", "zip"), value = "value",
+                borderWidth = 0) %>% 
+  hc_colorAxis(dataClasses = color_classes(brks, colors)) %>% 
+  # functionality
+  hc_tooltip(headerFormat = "",
+             pointFormat = "{point.zip}: {point.value}",
+             valueDecimals = 2) %>% 
+  hc_legend(align = "right", verticalAlign = "bottom", layout = "vertical",
+            floating = TRUE) %>%
+  hc_mapNavigation(enabled = FALSE) %>% # if TRUE to zoom the relief image dont zoom.
+  # info
+  hc_title(text = "Pediatric Asthma Self-Report") %>% 
+  hc_subtitle(text = "Rate per 100") %>% 
+  hc_credits(enabled = TRUE,
+             text = "CHIS, 2014") %>% 
+  # style
+  hc_chart(backgroundColor = "transparent",
+           events = list(
+             load = JS("function(){ $(\"image\")[0].setAttribute('preserveAspectRatio', 'xMidYMid') }")
+           ))
